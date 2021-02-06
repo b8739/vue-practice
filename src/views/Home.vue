e3a72606bf8eb661e7b748a2603fa3f97a8e27d2
b8739 <b8739@hanmail.net>
2021-02-06 15:45:38 +0900

    two way binding 예시

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,22 @@
 <template>
     <div>
         <h1>Welcome to {{title}}</h1>
+        <input type="text" v-model = "input1"> <!-- Vue에서는 value -> v-model-->
+        <button type="button" @click="getData">Get</button><!-- Vue에서는 onclick -> @click-->
     </div>
 </template>
 <script>
 export default {
     data(){
         return{
-            title:"JX님"
+            title:"JX님",
+            input1: "abc"
     };
 },
+    methods:{
+        getData(){
+            console.log(this.input1); //this를 붙여주어야 데이터에 접근 가능
+        }
+    }
 }
 </script>
