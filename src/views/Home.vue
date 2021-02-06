e052e78343d9f2e2d6162f857ef6a1cf060c0e5b
b8739 <b8739@hanmail.net>
2021-02-06 15:48:41 +0900

    two way binding 예시_setData함수

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
         <h1>Welcome to {{title}}</h1>
         <input type="text" v-model = "input1"> <!-- Vue에서는 value -> v-model-->
         <button type="button" @click="getData">Get</button><!-- Vue에서는 onclick -> @click-->
+        <button type="button" @click="setData">Set</button><!-- Vue에서는 onclick -> @click-->
     </div>
 </template>
 <script>
@@ -16,6 +17,9 @@ export default {
     methods:{
         getData(){
             console.log(this.input1); //this를 붙여주어야 데이터에 접근 가능
+        },
+        setData(){
+            this.input1="12345";
         }
     }
 }
